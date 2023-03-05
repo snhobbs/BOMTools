4c41cdf800e35d170d486bf60cd0df0ef94c8a33
simon <simon.hobbs@electrooptical.net>
2023-03-05 14:17:44 -0500

    tweaks

--- bin/jlc_part_lookup.py
+++ bin/jlc_part_lookup.py
@@ -13,19 +13,22 @@ ordering = ("Manufacturer", "Package","MFR.Part", "Solder Joint", "Description")
 @click.option("--bom", "-b", default="BOM.csv")
 @click.option("--out", "-o", default=None)
 @click.option("--save", "-s", type=bool, default=True)
-def command(database, bom, out, save):
+@click.option("--pseudonyms", "-p", type=str, default="", help="Alternative column names in json format")
+def command(database, bom, out, save, pseudonyms):
     bom = os.path.realpath(bom)
     if out is None:
         split = bom.split(".")
         out = f"{split[0]}_Expanded.xlsx"
     else:
         save = True
-    pseudonyms = '{"LCSC Part": ["LCSC", "JLC", "JLCPCB"]}'
     df = read_file_to_df(bom)
     pseudonyms=read_pseodonyms(pseudonyms)
     left = extract_columns_by_pseudonyms(df, pseudonyms)
     right = extract_columns_by_pseudonyms(df, pseudonyms)
-    assert("LCSC Part" in df.columns)
+
+    join_column = "LCSC Part"
+    print(df.columns)
+    assert(join_column in df.columns)
 
     # initialize to empty columns
     for column in ordering:
@@ -38,13 +41,15 @@ def command(database, bom, out, save):
     selection = ",".join(["\"{}\"".format(p) for p in ordering])
     print(selection)
     for _, line in df.iterrows():
-        command = "select {} from parts where \"LCSC Part\"=\"{}\"".format(selection, line["LCSC Part"])
+        join_value = line[join_column]
+        command = f"select {selection} from parts where \"{join_column}\"=\"{join_value}\""
         print(command)
-        data = db.execute(command)
-        data = list(data)[0]
-        print(data)
-        for column, value in zip(ordering, data):
-            line[column] = value
+        data_read = list(db.execute(command))
+        if data_read:
+            data = data_read[0]
+            print(data)
+            for column, value in zip(ordering, data):
+                line[column] = value
 
     if save:
         df.to_excel(out, index=False)
